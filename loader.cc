082847f148428c01046b86cb4f6c502d61905aaf
raster <pektyer305@gmail.com>
2025-11-17 00:24:44 +0900

    Refactor open_bfd function in loader.cc to improve error handling and readability

--- loader.cc
+++ loader.cc
@@ -13,6 +13,42 @@
 
 using namespace std;
 
+// libbfdを初期化して、指定されたファイル名のBFDハンドルを開く
+static bfd *open_bfd(string &fname)
+{
+    static int bfd_inited = 0;
+
+    bfd *bfd_handler;
+
+    if (!bfd_inited)
+    {
+        bfd_init();
+        bfd_inited = 1;
+    }
+
+    bfd_handler = bfd_openr(fname.c_str(), NULL); // fnameをC++からC文字列に変換して渡す
+    if (!bfd_handler)
+    {
+        fprintf(stderr, "Error: failed to open binary '%s': %s\n", fname.c_str(), bfd_errmsg(bfd_get_error()));
+        return NULL;
+    }
+
+    if (!bfd_check_format(bfd_handler, bfd_object))
+    {
+        fprintf(stderr, "file '%s' does not look like an executable (%s)\n", fname.c_str(), bfd_errmsg(bfd_get_error()));
+        return NULL;
+    }
+
+    bfd_set_error(bfd_error_no_error); // エラー状態をクリア
+
+    if (bfd_get_flavour(bfd_handler) == bfd_target_unknown_flavour)
+    {
+        fprintf(stderr, "unrecognized binary format '%s' (%s)\n", fname.c_str(), bfd_errmsg(bfd_get_error()));
+        return NULL;
+    }
+    return bfd_handler;
+}
+
 int load_binary(string &fname, Binary *bin, Binary::BinaryType bintype)
 {
     return load_binary_bfd(fname, bin, bintype);
