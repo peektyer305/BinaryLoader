6cb0d96a288c8d7ce1e6c3957e842754e504aef3
raster <pektyer305@gmail.com>
2025-11-16 23:14:59 +0900

    Implement unload_binary function to free allocated memory for binary sections

--- loader.cc
+++ loader.cc
@@ -17,3 +17,19 @@ int load_binary(string &fname, Binary *bin, Binary::BinaryType bintype)
 {
     return load_binary_bfd(fname, bin, bintype);
 }
+
+void unload_binary(Binary *bin)
+{
+    size_t i;
+    Section *sec;
+
+    for (i = 0; i < bin->sections.size(); i++)
+    {
+        sec = &bin->sections[i];
+        if (sec->bytes)
+        {
+            free(sec->bytes);
+            sec->bytes = NULL;
+        }
+    }
+}
