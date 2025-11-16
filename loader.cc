9aef8b97e1ee3b7299acd80035444d4a51fa6cc6
raster <pektyer305@gmail.com>
2025-11-16 16:56:00 +0900

    Add implementation of loader.cc with necessary includes

---
+++ loader.cc
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+
+#include <string>
+#include <vector>
+
+#include <bfd.h>
+
+#include "loader.h"
+
+static bfd *
