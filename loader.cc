8861d2e2f251bbce89cfe0cb55fbda7e16eb487a
raster <pektyer305@gmail.com>
2025-11-16 23:08:10 +0900

    Refactor open_bfd function in loader.cc to integrate its logic into load_binary function for improved clarity and maintainability

--- loader.cc
+++ loader.cc
@@ -13,38 +13,7 @@
 
 using namespace std;
 
-static bfd *open_bfd(string &fname)
+int load_binary(string &fname, Binary *bin, Binary::BinaryType bintype)
 {
-    static int bfd_inited = 0;
-
-    bfd *bfd_h;
-
-    if (!bfd_inited)
-    {
-        bfd_init();
-        bfd_inited = 1;
-    }
-
-    bfd_h = bfd_openr(fname.c_str(), NULL);
-    if (!bfd_h)
-    {
-        fprintf(stderr, "failed to open binary '%s' (%s)¥n", fname.c_str(), bfd_errmsg(bfd_get_error()));
-        return NULL;
-    }
-
-    if (!bfd_check_format(bfd_h, bfd_object))
-    {
-        fprintf(stderr, "file '%s' does not look like an executable (%s)¥n", fname.c_str(), bfd_errmsg(bfd_get_error()));
-        bfd_close(bfd_h);
-        return NULL;
-    }
-
-    bfd_set_error(bfd_error_no_error);
-
-    if (bfd_get_flavour(bfd_h) == bfd_target_unknown_flavour)
-    {
-        fprintf(stderr, "unknown binary format: '%s' (%s)¥n", fname.c_str(), bfd_errmsg(bfd_get_error()));
-        return NULL;
-    }
-    return bfd_h;
+    return load_binary_bfd(fname, bin, bintype);
 }
